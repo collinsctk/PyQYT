d4c19e68e796114c896d8e006dd0bb8d818921c0
collinsctk <collinsctk@qytang.com>
2016-05-04 17:19:03 +0800

    traceroute

--- Network/ICMP/Traceroute.py
+++ Network/ICMP/Traceroute.py
@@ -15,44 +15,46 @@ import random
 import sys
 import re
 
-def Tracert_one(dst,dport,ttl_no):
-	send_time = time.time()
+def Tracert_one(dst,dport,ttl_no):#发一个Traceroute包，参数需要目的地址，目的端口，TTL。
+	send_time = time.time()#记录发送时间
 	Tracert_one_reply = sr1(IP(dst=dst, ttl=ttl_no)/UDP(dport=dport)/b'qytang traceroute!!!', timeout = 1, verbose=False)
 	try:
 		if Tracert_one_reply.getlayer(ICMP).type == 11 and Tracert_one_reply.getlayer(ICMP).code == 0:
+			#如果收到TTL超时
 			hop_ip = Tracert_one_reply.getlayer(IP).src
 			received_time = time.time()
 			time_to_passed = (received_time - send_time) * 1000
-			return 1, hop_ip, time_to_passed
+			return 1, hop_ip, time_to_passed #返回1表示并未抵达目的地
 		elif Tracert_one_reply.getlayer(ICMP).type == 3 and Tracert_one_reply.getlayer(ICMP).code == 3:
+			#如果收到端口不可达
 			hop_ip = Tracert_one_reply.getlayer(IP).src
 			received_time = time.time()
 			time_to_passed = (received_time - send_time) * 1000
-			return 2, hop_ip, time_to_passed
+			return 2, hop_ip, time_to_passed #返回2表示抵达目的地
 	except Exception as e:
 		if re.match('.*NoneType.*',str(e)):
-			return None
+			return None #测试失败返回None
 
 def QYT_Tracert(dst,hops):
-	dport = 33434
+	dport = 33434 #Traceroute的目的端口从33434开始计算
 	hop = 0
 	while hop < hops:
 		dport = dport + hop
 		hop += 1
 		Result = Tracert_one(dst,dport,hop)
-		if Result == None:
+		if Result == None:#如果测试失败就打印‘*’
 			print(str(hop) + ' *',flush=True)
-		elif Result[0] == 1:
+		elif Result[0] == 1:#如果未抵达目的，就打印这一跳和消耗的时间
 			time_to_pass_result = '%4.2f' % Result[2]
 			print(str(hop) + ' ' + str(Result[1]) + ' ' + time_to_pass_result + 'ms')
-		elif Result[0] == 2:
+		elif Result[0] == 2:#如果抵达目的，就打印这一跳和消耗的时间，并且跳出循环！
 			time_to_pass_result = '%4.2f' % Result[2]
 			print(str(hop) + ' ' + str(Result[1]) + ' ' + time_to_pass_result + 'ms')
 			break
 		time.sleep(1)
 
 if __name__ == '__main__':
-	conf.route.add(net='202.100.0.0/16',gw='202.100.1.3')
+	conf.route.add(net='202.100.0.0/16',gw='202.100.1.3')#为Scapy添加路由
 	destination = sys.argv[1]
 	hops = int(sys.argv[2])
 	QYT_Tracert(destination, hops)
