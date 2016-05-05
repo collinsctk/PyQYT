ce9bda6f6aed8135f7a1d7b4b1cc643b22d041d9
collinsctk <collinsctk@qytang.com>
2016-05-05 16:02:17 +0800

    dns

--- Network/DNS/DNS_Query.py
+++ Network/DNS/DNS_Query.py
@@ -12,12 +12,14 @@ from scapy.all import *
 import re
 
 def dns_query(dns_name):
-	dns_result = sr1(IP(dst="114.114.114.114")/UDP()/DNS(rd=1,qd=DNSQR(qname=dns_name)), verbose=False)
+	dns_result = sr1(IP(dst="114.114.114.114")/UDP()/DNS(id=168,qr=0,opcode=0,rd=1,qd=DNSQR(qname=dns_name)), verbose=False)
+	#id标识字段（匹配请求与回应），qr等于0表示查询报文，opcode为0表示标准查询，rd为1表示期望递归
 	layer = 1
 	while True:#不太确定DNSRR到底有几组！！！
 		try:
 			if dns_result.getlayer(DNS).fields['an'][layer].fields['type'] == 1: #A记录
 				dns_result_ip = dns_result.getlayer(DNS).fields['an'][layer].fields['rdata']
+				#每一层就是一个记录，但是不一定是A，可能是CNAME！
 				print('域名: %-18s 对应的IP地址: %s' % (dns_name, dns_result_ip))#找到IP地址并打印
 			layer += 1
 		except:#如果超出范围就跳出循环
