e23f9b1135006917f547d9fc56f83ba2a39c75fa
collinsctk <collinsctk@qytang.com>
2016-05-06 09:26:07 +0800

    mygetnext

--- Network/SNMP/mygetnext.py
+++ Network/SNMP/mygetnext.py
@@ -9,7 +9,6 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 import sys
 from io import StringIO
 
-vallist = []
 cmdGen = cmdgen.CommandGenerator()
 
 def snmpv3_getnext(ip='',user='',hash_meth=None,hash_key=None,cry_meth=None,cry_key=None,oid='',num=1):
@@ -25,8 +24,7 @@ def snmpv3_getnext(ip='',user='',hash_meth=None,hash_key=None,cry_meth=None,cry_
     hashval = None
     cryval = None
 
-    global vallist
-    vallist = [None]*num
+    #========================下面的操作在判断安全模型==========================
     #NoAuthNoPriv
     if hash_meth == None and cry_meth == None:
         hashval = cmdgen.usmNoAuthProtocol
@@ -67,20 +65,20 @@ def snmpv3_getnext(ip='',user='',hash_meth=None,hash_key=None,cry_meth=None,cry_
     else:
     	print('三种USM: NoAuthNoPriv, AuthNoPriv, AuthPriv.。请选择其中一种。')
     	return
-
+    #========================判断安全模型结束==========================
     errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
         cmdgen.UsmUserData(user, hash_key, cry_key,
                            authProtocol=hashval,
-                           privProtocol=cryval),
-        cmdgen.UdpTransportTarget((ip, 161)),
-        oid,
-        lexicographicMode=True, maxRows=num, ignoreMonIncreasingOid=True
+                           privProtocol=cryval),#添加用户，散列密钥，加密密钥，散列协议，加密协议
+        cmdgen.UdpTransportTarget((ip, 161)),#添加目标地址和端口号
+        oid,#指定oid
+        lexicographicMode=True, maxRows=num, ignoreMonIncreasingOid=True#指定最大行数
     )
 
-    if errorIndication:
+    if errorIndication:#打印错误
         print(errorIndication)
     else:
-        if errorStatus:
+        if errorStatus:#打印错误
             print('%s at %s' % (
                 errorStatus.prettyPrint(),
                 errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
@@ -92,28 +90,28 @@ def snmpv3_getnext(ip='',user='',hash_meth=None,hash_key=None,cry_meth=None,cry_
                 for oid, val in varBindTableRow:
                     o = StringIO()
                     print(oid,file=o)
-                    oid_get = o.getvalue().strip()
+                    oid_get = o.getvalue().strip()#通过print到StringIO进行转码，然后读回
                     o.close()
                     v = StringIO()
                     print(val,file=v)
-                    val_get = v.getvalue().strip()
+                    val_get = v.getvalue().strip()#通过print到StringIO进行转码，然后读回
                     v.close()
-                    oid_list.append((oid_get,val_get))
+                    oid_list.append((oid_get,val_get))#添加oid和对应值的信息到oid_list
 
-    return(oid_list)
+    return(oid_list)#返回oid_list
 
 if __name__ == '__main__':
     try:
-        ip = sys.argv[1]
-        user = sys.argv[2]
-        hm = sys.argv[3]
-        hk = sys.argv[4]
-        cm = sys.argv[5]
-        ck = sys.argv[6]
-        oid = sys.argv[7]
-        num = int(sys.argv[8])
+        ip = sys.argv[1]#读取客户输入参数
+        user = sys.argv[2]#读取客户输入参数
+        hm = sys.argv[3]#读取客户输入参数
+        hk = sys.argv[4]#读取客户输入参数
+        cm = sys.argv[5]#读取客户输入参数
+        ck = sys.argv[6]#读取客户输入参数
+        oid = sys.argv[7]#读取客户输入参数
+        num = int(sys.argv[8])#读取客户输入参数
         for item in snmpv3_getnext(ip,user,hm,hk,cm,ck,oid,num):
-            print('OID: ', item[0], 'VALUE: ', item[1])
+            print('OID: ', item[0], 'VALUE: ', item[1])#从oid_list读取并且打印信息
 
     except Exception as e:
         #print(e)
