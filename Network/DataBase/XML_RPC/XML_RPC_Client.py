a91de2186263f664fb9d376d02fbcc2dddc64d3b
collinsctk <collinsctk@qytang.com>
2016-05-10 09:57:49 +0800

    ‘XML.rpc’

---
+++ Network/DataBase/XML_RPC/XML_RPC_Client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.4
+# -*- coding=utf-8 -*-
+#本脚由亁颐堂现任明教教主编写，用于乾颐盾Python课程！
+#教主QQ:605658506
+#亁颐堂官网www.qytang.com
+#乾颐盾是由亁颐堂现任明教教主开发的综合性安全课程
+#包括传统网络安全（防火墙，IPS...）与Python语言和黑客渗透课程！
+
+#参考原始文档
+#https://docs.python.org/3.1/library/xmlrpc.client.html#example-of-client-and-server-usage
+
+import datetime
+import xmlrpc.client
+
+proxy = xmlrpc.client.ServerProxy("http://202.100.1.138:8000/")
+print("3 is even: %s" % str(proxy.is_even(3)))#远程执行proxy上的is_even函数
+print("100 is even: %s" % str(proxy.is_even(100)))#远程执行proxy上的is_even函数
+
+today = proxy.today()#远程执行proxy上的today函数
+#convert the ISO8601 string to a datetime object
+converted = datetime.datetime.strptime(today.value, "%Y%m%dT%H:%M:%S")
+print("Today: %s" % converted.strftime("%d.%m.%Y, %H:%M"))
+
+dst = '202.100.1.139'
+ping_result = proxy.ping(dst,1,1,64)
+if ping_result:
+	print(dst + ' Ping测试通过！')
---
+++ Network/DataBase/XML_RPC/XML_RPC_Server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3.4
+# -*- coding=utf-8 -*-
+#本脚由亁颐堂现任明教教主编写，用于乾颐盾Python课程！
+#教主QQ:605658506
+#亁颐堂官网www.qytang.com
+#乾颐盾是由亁颐堂现任明教教主开发的综合性安全课程
+#包括传统网络安全（防火墙，IPS...）与Python语言和黑客渗透课程！
+
+#参考原始文档
+#https://docs.python.org/3.1/library/xmlrpc.client.html#example-of-client-and-server-usage
+
+import datetime
+from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
+from PyQYT.Network.ICMP.PING import ping_one
+
+def is_even(n):#创建函数，判断奇数偶数
+    return n%2 == 0
+
+def today():#创建函数，返回时间
+    today = datetime.datetime.today()
+    return xmlrpc.client.DateTime(today)
+
+server = SimpleXMLRPCServer(("202.100.1.138", 8000))
+print("Listening on port 8000...")
+server.register_multicall_functions()#启动多函数注册功能！
+server.register_function(is_even, "is_even")#注册函数，名字为"is_even"
+server.register_function(today, "today")#注册函数，名字为"today"
+server.register_function(ping_one, "ping")#注册函数，名字为"today"
+server.serve_forever()#运行服务器
