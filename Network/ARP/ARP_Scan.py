0a1509c1bac490ba3d8dcf58f392204e62935a18
collinsctk <collinsctk@qytang.com>
2016-05-16 22:35:02 -0400

    Update ARP_Scan.py

--- Network/ARP/ARP_Scan.py
+++ Network/ARP/ARP_Scan.py
@@ -14,8 +14,8 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)#清除报错
 from scapy.all import *
 #from GET_IP import get_ip_address #导入获取本机IP地址方法
-from GET_IP_IFCONFIG import get_ip_address_ifconfig #导入获取本机IP地址方法
-from GET_MAC import get_mac_address #导入获取本机MAC地址方法
+from PyQYT.Network.Tools.GET_IP_IFCONFIG import get_ip_address_ifconfig #导入获取本机IP地址方法
+from PyQYT.Network.Tools.GET_MAC import get_mac_address #导入获取本机MAC地址方法
 import optparse
 
 def arp_scan(network_prefix, ifname):
